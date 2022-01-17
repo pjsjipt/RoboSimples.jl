02ede2fad34aa937544b35ba8f5b354a12fc52ef
Paulo José Saiz Jabardo <pjabardo@ipt.br>
2022-01-17 15:08:28 -0300

    Moved from AbstractMover to AbstractActuator
    Using AbstractCartesianRobot as parent for NRoboClient

--- src/RoboSimples.jl
+++ src/RoboSimples.jl
@@ -1,7 +1,7 @@
 module RoboSimples
 
 using PyCall
-using AbstractMover
+using AbstractActuator
 
 export NRoboClient
 export move, moveX, moveY, moveZ, position, setreference
@@ -11,14 +11,14 @@ export setreferenceX, setreferenceY, setreferenceZ
 export numaxes 
 
 
-struct NRoboClient <: AbstractMoverDev
+struct NRoboClient <: AbstractCartesianRobot
     ip::String
     port::Int32
     server::PyObject
 end
 
-AbstractMover.numaxes(dev::NRoboClient) = 3
-AbstractMover.numaxes(::Type{NRoboClient}) = 3
+AbstractActuator.numaxes(dev::NRoboClient) = 3
+AbstractActuator.numaxes(::Type{NRoboClient}) = 3
 
 function NRoboClient(ip="192.168.0.140", port=9543)
     xmlrpc = pyimport("xmlrpc.client")
@@ -27,19 +27,19 @@ function NRoboClient(ip="192.168.0.140", port=9543)
 end
 
 
-AbstractMover.move(dev::NRoboClient, mm, ax="z"; r=true) =
+AbstractActuator.move(dev::NRoboClient, mm, ax="z"; r=true) =
     dev.server["move"](mm, string(ax), r)
 
-AbstractMover.moveX(dev::NRoboClient, mm) = dev.server["moveX"](mm)
-AbstractMover.moveY(dev::NRoboClient, mm) = dev.server["moveY"](mm)
-AbstractMover.moveZ(dev::NRoboClient, mm) = dev.server["moveZ"](mm)
+AbstractActuator.moveX(dev::NRoboClient, mm) = dev.server["moveX"](mm)
+AbstractActuator.moveY(dev::NRoboClient, mm) = dev.server["moveY"](mm)
+AbstractActuator.moveZ(dev::NRoboClient, mm) = dev.server["moveZ"](mm)
 
-AbstractMover.rmoveX(dev::NRoboClient, mm) = dev.server["rmoveX"](mm)
-AbstractMover.rmoveY(dev::NRoboClient, mm) = dev.server["rmoveY"](mm)
-AbstractMover.rmoveZ(dev::NRoboClient, mm) = dev.server["rmoveZ"](mm)
+AbstractActuator.rmoveX(dev::NRoboClient, mm) = dev.server["rmoveX"](mm)
+AbstractActuator.rmoveY(dev::NRoboClient, mm) = dev.server["rmoveY"](mm)
+AbstractActuator.rmoveZ(dev::NRoboClient, mm) = dev.server["rmoveZ"](mm)
 
 import Base
-function AbstractMover.position(dev::NRoboClient; pulses=false)
+function AbstractActuator.position(dev::NRoboClient; pulses=false)
     x = dev.server["position"]("x", pulses)
     y = dev.server["position"]("y", pulses)
     z = dev.server["position"]("z", pulses)
@@ -47,26 +47,26 @@ function AbstractMover.position(dev::NRoboClient; pulses=false)
     return Dict{String,Float64}("x"=>x, "y"=>y, "z"=>z)
 end
 
-AbstractMover.position(dev::NRoboClient, ax; pulses=false) =
+AbstractActuator.position(dev::NRoboClient, ax; pulses=false) =
     dev.server["position"](string(ax), pulses)
 
-AbstractMover.positionX(dev::NRoboClient; pulses=false) =
+AbstractActuator.positionX(dev::NRoboClient; pulses=false) =
     position(dev, "x"; pulses=pulses)
-AbstractMover.positionY(dev::NRoboClient; pulses=false) =
+AbstractActuator.positionY(dev::NRoboClient; pulses=false) =
     position(dev, "y"; pulses=pulses)
-AbstractMover.positionZ(dev::NRoboClient; pulses=false) =
+AbstractActuator.positionZ(dev::NRoboClient; pulses=false) =
     position(dev, "z"; pulses=pulses)
 
-AbstractMover.setreference(dev::NRoboClient, ax, mm=0; pulses=false) =
+AbstractActuator.setreference(dev::NRoboClient, ax, mm=0; pulses=false) =
     dev.server["set_reference"](string(ax), mm, pulses)
 
-AbstractMover.setreferenceX(dev::NRoboClient, mm=0 ; pulses=false) =
+AbstractActuator.setreferenceX(dev::NRoboClient, mm=0 ; pulses=false) =
     setreference(dev, "x", mm; pulses=pulses)
 
-AbstractMover.setreferenceY(dev::NRoboClient, mm=0 ; pulses=false) =
+AbstractActuator.setreferenceY(dev::NRoboClient, mm=0 ; pulses=false) =
     setreference(dev, "y", mm; pulses=pulses)
 
-AbstractMover.setreferenceZ(dev::NRoboClient, mm=0 ; pulses=false) =
+AbstractActuator.setreferenceZ(dev::NRoboClient, mm=0 ; pulses=false) =
     setreference(dev, "z", mm; pulses=pulses)
 
 
